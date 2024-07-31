d16e5a9bd1ec66a2b6c04bdf94da3e12e9191189
olaoluwiv <ayidinao@gmail.com>
2024-07-31 08:11:14 +0000

    update

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,4 @@
-export default function createInt8TypedArray(length, position, value) {
+function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
   
@@ -10,3 +10,5 @@ export default function createInt8TypedArray(length, position, value) {
   
   return buffer;
 }
+
+module.exports = createInt8TypedArray;
